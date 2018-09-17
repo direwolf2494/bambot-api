1ed41ccafedca65c008dbc19b36fd42c5f754925
Shervain Barrett <jay_94@live.com>
2018-09-17 16:58:17 -0500

    my first commit

---
+++ app/app.ts
@@ -0,0 +1,21 @@
+import * as express from 'express';
+import * as config from 'config';
+import * as bodyParser from 'body-parser';
+
+const app = express();
+// configure body-parser
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// default slackbot endpoint
+app.post('/api/v1/slackbot', (req, res) => {
+  res.status(200).send({
+    challenge: req.body.challenge
+  });
+});
+
+const PORT = 5000;
+
+app.listen(PORT, () => {
+  console.log(`server running on port ${PORT}`)
+});
