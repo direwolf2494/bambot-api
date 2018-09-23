e6df4a288289fa4c05c6bbeab4c4c56d8c8bb044
Shervain Barrett <jay_94@live.com>
2018-09-23 13:53:11 -0500

    now using external redis server

--- app/utils/redis.ts
+++ app/utils/redis.ts
@@ -2,5 +2,8 @@ import * as bluebird from 'bluebird';
 import * as redis from 'redis';
 
 let redisAsync = bluebird.promisifyAll(redis);
-let redisClient = redisAsync.createClient();
+let redisClient = redisAsync.createClient({
+    url: process.env.REDIS_URL,
+    password: process.env.REDIS_PASSWORD
+});
 export default redisClient;
